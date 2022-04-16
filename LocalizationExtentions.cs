a0890c5bdcac81c3fb36affe2565d6ed2336c3d7
Luigi <Luigi@DSKTP-DXB-LUIGI>
2022-04-16 10:28:30 +0400

    null checks

--- LocalizationExtentions.cs
+++ LocalizationExtentions.cs
@@ -6,97 +6,98 @@ using Utilities.String.Extentions;
 
 namespace Utilities.Localization.Extentions
 {
-	public static class LocalizationExtentions
-	{
-		static ResourceManager mng = null;
-		readonly static Dictionary<string, CultureInfo> cultures = new Dictionary<string, CultureInfo>();
-		readonly static object sync = new object();
-		/// <summary>
-		/// Gets or sets the resource manager used by Translate to localize
-		/// </summary>
-		public static ResourceManager ResourceManager { get => mng; set => mng = value; }
-		public static string Translate(this ResourceManager rscmng, FormattableString str, CultureInfo culture)
-		{
-			if (rscmng == null || culture == null || str == null || string.IsNullOrWhiteSpace(str.ToString())) return str?.ToString();
-			var transl = rscmng.GetString(str.Format, culture);
-			if (string.IsNullOrWhiteSpace(transl)) return str.ToString();
-			return transl.Format(culture, str.GetArguments());
-		}
-		public static string Translate(this FormattableString str, CultureInfo culture)
-		{
-			if (culture == null || str == null || string.IsNullOrEmpty(str.ToString())) return str?.ToString();
-			if (mng == null)
-				throw new InvalidOperationException("Set LocalizationExtentions.ResourceManager before calling this method");
-			return Translate(mng, str, culture);
-		}
-		public static string TranslateString(this ResourceManager rscmng, CultureInfo culture, string str, params object[] args)
-		{
-			if (rscmng == null || culture == null || str == null || string.IsNullOrWhiteSpace(str))
-				return str?.Format(culture, args);
-			var transl = rscmng.GetString(str, culture);
-			if (string.IsNullOrWhiteSpace(transl)) transl = str;
-			return transl.Format(culture, args);// string.Format(rscmng.GetString(str, culture), arguments) ?? str;
-		}
-		public static string Translate(this string str, CultureInfo culture, params object[] arguments)
-		{
-			return TranslateString(mng, culture, str, arguments);
-			//if (culture == null || str == null || string.IsNullOrEmpty(str)) return str;
-			//if (mng == null)
-			//	throw new InvalidOperationException("Set LocalizationExtentions.ResourceManager before calling this method");
-			//return TranslateString(mng, culture, str, arguments);
-		}
-		public static string TranslateString(this CultureInfo culture, string str, params object[] arguments)
-		{
-			return TranslateString(mng, culture, str, arguments);
-			//if (str == null || string.IsNullOrWhiteSpace(str)) return str;
-			//if (culture == null) return str.Format(arguments);
-			//if (mng == null)
-			//	throw new InvalidOperationException("Set LocalizationExtentions.ResourceManager before calling this method");
-			//return string.Format(mng.GetString(str, culture), arguments) ?? str;
-		}
-		public static string Translate(this CultureInfo culture, FormattableString str)
-		{
-			if (culture == null || str == null || string.IsNullOrEmpty(str.ToString())) return str.ToString();
-			if (mng == null)
-				throw new InvalidOperationException("Set LocalizationExtentions.ResourceManager before calling this method");
-			return Translate(mng, str, culture);
-		}
-		public static string Translate(this ResourceManager rscmng, FormattableString str, string culture)
-		{
-			if (rscmng == null || string.IsNullOrEmpty(culture) || str == null || string.IsNullOrWhiteSpace(str.ToString())) return str?.ToString();
-			return TranslateString(rscmng, GetOrAddCulture(culture), str.Format, str.GetArguments());
-		}
-		public static string Translate(this FormattableString str, string culture)
-		{
-			if (string.IsNullOrEmpty(culture) || str == null || string.IsNullOrEmpty(str.ToString())) return str?.ToString();
-			if (mng == null)
-				throw new InvalidOperationException("Set LocalizationExtentions.ResourceManager before calling this method");
-			return Translate(mng, str, culture);
-		}
-		public static string TranslateString(this ResourceManager rscmng, string culture, string str, params object[] arguments)
-		{
-			if (string.IsNullOrEmpty(str)) return str;
-			if (string.IsNullOrEmpty(culture) || rscmng == null) return str.Format(arguments) ?? str;
-			return TranslateString(mng, GetOrAddCulture(culture), str, arguments);
-		}
-		public static string Translate(this string str, string culture, params object[] arguments)
-		{
-			if (string.IsNullOrEmpty(str)) return str;
-			if (string.IsNullOrEmpty(culture)) return str.Format(arguments) ?? str;
-			if (mng == null)
-				throw new InvalidOperationException("Set LocalizationExtentions.ResourceManager before calling this method");
-			return TranslateString(mng, GetOrAddCulture(culture), str, arguments);
-		}
-		static CultureInfo GetOrAddCulture(string culture)
-		{
-			if (string.IsNullOrWhiteSpace(culture)) return null;
-			lock (sync)
-			{
-				if (cultures.ContainsKey(culture)) return cultures[culture];
-				var ci = new CultureInfo(culture);
-				cultures.Add(culture, ci);
-				return ci;
-			}
-		}
-	}
+    public static class LocalizationExtentions
+    {
+        static ResourceManager? mng = null;
+        readonly static Dictionary<string, CultureInfo> cultures = new Dictionary<string, CultureInfo>();
+        readonly static object sync = new object();
+        /// <summary>
+        /// Gets or sets the resource manager used by Translate to localize
+        /// </summary>
+        public static ResourceManager? ResourceManager { get => mng; set => mng = value; }
+        public static string? Translate(this ResourceManager rscmng, FormattableString str, CultureInfo culture)
+        {
+            if (rscmng == null || culture == null || str == null || string.IsNullOrWhiteSpace(str.ToString())) return str?.ToString();
+            var transl = rscmng.GetString(str.Format, culture);
+            if (string.IsNullOrWhiteSpace(transl)) return str.ToString();
+            object[] args = (object[])(str.GetArguments() ?? Array.Empty<object>());
+            return transl.Format(culture, args);
+        }
+        public static string? Translate(this FormattableString str, CultureInfo culture)
+        {
+            if (culture == null || str == null || string.IsNullOrEmpty(str.ToString())) return str?.ToString();
+            if (mng == null)
+                throw new InvalidOperationException("Set LocalizationExtentions.ResourceManager before calling this method");
+            return Translate(mng, str, culture);
+        }
+        public static string? TranslateString(this ResourceManager? rscmng, CultureInfo? culture, string? str, params object[] args)
+        {
+            if (rscmng == null || culture == null || str == null || string.IsNullOrWhiteSpace(str))
+                return culture == null ? str : str?.Format(culture, args);
+            var transl = rscmng.GetString(str, culture);
+            if (string.IsNullOrWhiteSpace(transl)) transl = str;
+            return transl.Format(culture, args);// string.Format(rscmng.GetString(str, culture), arguments) ?? str;
+        }
+        public static string? Translate(this string str, CultureInfo culture, params object[] arguments)
+        {
+            return TranslateString(mng, culture, str, arguments);
+            //if (culture == null || str == null || string.IsNullOrEmpty(str)) return str;
+            //if (mng == null)
+            //	throw new InvalidOperationException("Set LocalizationExtentions.ResourceManager before calling this method");
+            //return TranslateString(mng, culture, str, arguments);
+        }
+        public static string? TranslateString(this CultureInfo culture, string str, params object[] arguments)
+        {
+            return TranslateString(mng, culture, str, arguments);
+            //if (str == null || string.IsNullOrWhiteSpace(str)) return str;
+            //if (culture == null) return str.Format(arguments);
+            //if (mng == null)
+            //	throw new InvalidOperationException("Set LocalizationExtentions.ResourceManager before calling this method");
+            //return string.Format(mng.GetString(str, culture), arguments) ?? str;
+        }
+        public static string? Translate(this CultureInfo culture, FormattableString str)
+        {
+            if (culture == null || str == null || string.IsNullOrEmpty(str.ToString())) return str.ToString();
+            if (mng == null)
+                throw new InvalidOperationException("Set LocalizationExtentions.ResourceManager before calling this method");
+            return Translate(mng, str, culture);
+        }
+        public static string? Translate(this ResourceManager rscmng, FormattableString str, string culture)
+        {
+            if (rscmng == null || string.IsNullOrEmpty(culture) || str == null || string.IsNullOrWhiteSpace(str.ToString())) return str?.ToString();
+            return TranslateString(rscmng, GetOrAddCulture(culture), str.Format, str.GetArguments());
+        }
+        public static string? Translate(this FormattableString str, string culture)
+        {
+            if (string.IsNullOrEmpty(culture) || str == null || string.IsNullOrEmpty(str.ToString())) return str?.ToString();
+            if (mng == null)
+                throw new InvalidOperationException("Set LocalizationExtentions.ResourceManager before calling this method");
+            return Translate(mng, str, culture);
+        }
+        public static string? TranslateString(this ResourceManager rscmng, string culture, string str, params object[] arguments)
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+            if (string.IsNullOrEmpty(culture) || rscmng == null) return str.Format(arguments) ?? str;
+            return TranslateString(mng, GetOrAddCulture(culture), str, arguments);
+        }
+        public static string? Translate(this string str, string culture, params object[] arguments)
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+            if (string.IsNullOrEmpty(culture)) return str.Format(arguments) ?? str;
+            if (mng == null)
+                throw new InvalidOperationException("Set LocalizationExtentions.ResourceManager before calling this method");
+            return TranslateString(mng, GetOrAddCulture(culture), str, arguments);
+        }
+        static CultureInfo? GetOrAddCulture(string culture)
+        {
+            if (string.IsNullOrWhiteSpace(culture)) return null;
+            lock (sync)
+            {
+                if (cultures.ContainsKey(culture)) return cultures[culture];
+                var ci = new CultureInfo(culture);
+                cultures.Add(culture, ci);
+                return ci;
+            }
+        }
+    }
 }
--- MillisecondsOrSecondsEpochConverter.cs
+++ MillisecondsOrSecondsEpochConverter.cs
@@ -4,22 +4,23 @@ using System;
 
 namespace Utilities.String.Json
 {
-	public class MillisecondsOrSecondsEpochConverter : DateTimeConverterBase
-	{
-		private static readonly DateTime _epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+    public class MillisecondsOrSecondsEpochConverter : DateTimeConverterBase
+    {
+        private static readonly DateTime _epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-		{
-			writer.WriteRawValue(((DateTime)value - _epoch).TotalMilliseconds.ToString("0"));
-		}
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteRawValue(((DateTime)value - _epoch).TotalMilliseconds.ToString("0"));
+        }
 
-		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-		{
-			if (reader.Value == null) { return null; }
-			if (reader.Value.ToString().Length > 10)
-				return _epoch.AddMilliseconds((long)reader.Value);
-			else
-				return _epoch.AddSeconds((long)reader.Value);
-		}
-	}
+        public override object? ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var s = reader.Value?.ToString();
+            if (s == null || !(reader.Value is long)) { return null; }
+            if (s.Length > 10)
+                return _epoch.AddMilliseconds((long)reader.Value);
+            else
+                return _epoch.AddSeconds((long)reader.Value);
+        }
+    }
 }
