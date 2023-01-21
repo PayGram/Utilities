86ae4f78f03440c374a7451774e83b4bd6036484
Luigi Menghini <menghini@gmail.com>
2023-01-21 14:39:29 +0400

    added semaphoreslimlock

---
+++ Collections/ILockProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Utilities.Collections
+{
+	public interface ILockProvider<T>
+	{
+		void Wait(T elementToLock);
+		Task WaitAsync(T elementToLock);
+		bool Release(T elementToLock, bool remove);
+	}
+}
---
+++ Collections/SemaphoreSlimLockProvider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Concurrent;
+
+namespace Utilities.Collections
+{
+    public class SemaphoreSlimLockProvider<T> : ILockProvider<T> where T : notnull
+    {
+        private static readonly ConcurrentDictionary<T, SemaphoreSlim> _lockDictionary = new();
+
+        public void Wait(T elementToLock)
+        {
+            _lockDictionary.GetOrAdd(
+                elementToLock, new SemaphoreSlim(1, 1)).Wait();
+        }
+
+        public async Task WaitAsync(T elementToLock)
+        {
+            await _lockDictionary.GetOrAdd(
+                elementToLock, new SemaphoreSlim(1, 1)).WaitAsync();
+        }
+
+        public bool Release(T elementToLock, bool remove = true)
+        {
+            if (_lockDictionary.TryGetValue(elementToLock, out var semaphore))
+            {
+                semaphore.Release();
+                if (remove)
+                    return _lockDictionary.TryRemove(elementToLock, out _);
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
