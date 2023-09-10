5dcf48e8630a4861ee9f58a46e43ca10833a4366
Luigi Menghini <menghini@gmail.com>
2023-09-11 03:04:23 +0400

    +inclueds red envelope redeemable coins in the the getCirculatingCoins +busineses +extended p2pgroups

---
+++ Collections/CollectionExts.cs
@@ -0,0 +1,24 @@
+﻿namespace Utilities.Collections
+{
+	public static class CollectionExts
+	{
+		private static Random rng = new Random();
+		/// <summary>
+		/// Shuffles a list
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="list"></param>
+		public static void Shuffle<T>(this IList<T> list)
+		{
+			int n = list.Count;
+			while (n > 1)
+			{
+				n--;
+				int k = rng.Next(n + 1);
+				T value = list[k];
+				list[k] = list[n];
+				list[n] = value;
+			}
+		}
+	}
+}
