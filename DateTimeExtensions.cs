7689d8c406a6a72284a11d30a83dea7882e88b31
Luigi Menghini <menghini@gmail.com>
2023-10-13 16:17:34 +0400

    p2p ads done businesses minor improvements

---
+++ DateTimeExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Utilities.String.Extentions
+{
+	public static class DateTimeExtensions
+	{
+		public static DateTime UtcToTimeZone(this DateTime utcDateTime, string timeZoneId)
+		{
+			var timeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+			return TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, timeZone);
+		}
+	}
+}
