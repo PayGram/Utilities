baf3b5e5268ceb32c13fef6996a795bcd9e5b0a1
Luigi Menghini <menghini@gmail.com>
2023-11-19 18:22:23 +0400

    update SemaphoreSlimLockProvider to auto clean up cache.

--- Collections/SemaphoreSlimLockProvider.cs
+++ Collections/SemaphoreSlimLockProvider.cs
@@ -1,34 +1,56 @@
 ﻿using System.Collections.Concurrent;
+using Utilities.Collections;
 
-namespace Utilities.Collections
+public class SemaphoreSlimThCount : SemaphoreSlim
 {
-    public class SemaphoreSlimLockProvider<T> : ILockProvider<T> where T : notnull
-    {
-        private static readonly ConcurrentDictionary<T, SemaphoreSlim> _lockDictionary = new();
+	public int MaxCount { get; private set; }
+	public int WaitingThreads { get; set; } = 0;
+	internal object Lock { get; private set; } = new object();
+	public SemaphoreSlimThCount(int initialCount, int maxCount) : base(initialCount, maxCount)
+	{
+		MaxCount = maxCount;
+	}
+}
+public class SemaphoreSlimLockProvider<T> : ILockProvider<T> where T : notnull
+{
+	private static readonly ConcurrentDictionary<T, SemaphoreSlimThCount> _lockDictionary = new();
 
-        public void Wait(T elementToLock)
-        {
-            _lockDictionary.GetOrAdd(
-                elementToLock, new SemaphoreSlim(1, 1)).Wait();
-        }
+	public void Wait(T elementToLock)
+	{
+		SemaphoreSlimThCount sem = _lockDictionary.GetOrAdd(elementToLock, new SemaphoreSlimThCount(1, 1));
+		lock (sem.Lock)
+		{
+			_lockDictionary.TryAdd(elementToLock, sem); // this is needed because the element just gotten might have removed concurrently by the release statement
+			sem.WaitingThreads++;
+		}
+		sem.Wait();
+	}
 
-        public async Task WaitAsync(T elementToLock)
-        {
-            await _lockDictionary.GetOrAdd(
-                elementToLock, new SemaphoreSlim(1, 1)).WaitAsync();
-        }
+	public async Task WaitAsync(T elementToLock)
+	{
+		SemaphoreSlimThCount sem = _lockDictionary.GetOrAdd(elementToLock, new SemaphoreSlimThCount(1, 1));
+		lock (sem.Lock)
+		{
+			_lockDictionary.TryAdd(elementToLock, sem); // this is needed because the element just gotten might have removed concurrently by the release statement
+			sem.WaitingThreads++;
+		}
+		await sem.WaitAsync();
+	}
 
-        public bool Release(T elementToLock, bool remove = true)
-        {
-            if (_lockDictionary.TryGetValue(elementToLock, out var semaphore))
-            {
-                semaphore.Release();
-                if (remove)
-                    return _lockDictionary.TryRemove(elementToLock, out _);
-                return true;
-            }
-            else
-                return false;
-        }
-    }
+	public bool Release(T elementToLock, bool remove = true)
+	{
+		SemaphoreSlimThCount? sem = _lockDictionary.GetValueOrDefault(elementToLock);
+		if (sem == null) return false;
+
+		lock (sem.Lock)
+		{
+			sem.WaitingThreads--;
+			sem.Release();
+			if (remove || sem.WaitingThreads == 0)
+			{
+				_lockDictionary.Remove(elementToLock, out _);
+			}
+		}
+		return true;
+	}
 }
